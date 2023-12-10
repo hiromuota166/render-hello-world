3d1960276191cc6ffd30ebec81f6d1fbb6a31a1f
hiromuota166 <otahiromu166@gmail.com>
2023-12-10 22:39:34 +0900

    戻した

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask
-from selenium import webdriver
-from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route('/')
-def hello_danjyo():
-    driver = webdriver.Chrome()
-    driver.get('https://www.reserve1.jp/yoyaku/member/MemberReserve.php?mn=2&lc=wrxqxwrrxx&gr=2&stt=MLRRX')
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser').text
-    return soup
+def hello_world():
+    return "Hello World!"
