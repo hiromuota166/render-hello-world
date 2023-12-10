da148cbe635c11976280af7cbe484ea51795c3c2
hiromuota166 <otahiromu166@gmail.com>
2023-12-10 23:04:37 +0900

    test

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    options = Options()
+    options.headless = True  # ヘッドレスモード True=非表示, False=表示
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.reserve1.jp/yoyaku/member/MemberReserve.php?mn=2&lc=wrxqxwrrxx&gr=2&stt=MLRRX")
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    return soup
