9c2fe6d2fbfc43141838a7ebc616d41500514ff5
hiromuota166 <otahiromu166@gmail.com>
2023-12-10 22:35:07 +0900

    test

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from selenium import webdriver
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def hello_danjyo():
+    driver = webdriver.Chrome()
+    driver.get('https://www.reserve1.jp/yoyaku/member/MemberReserve.php?mn=2&lc=wrxqxwrrxx&gr=2&stt=MLRRX')
 
-if __name__ == '__main__':
-    app.route(debug=True)
+    soup = BeautifulSoup(driver.page_source, 'html.parser').text
+    return soup
