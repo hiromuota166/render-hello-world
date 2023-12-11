3ded7f02bfc7d84293e595d29011c5026e4953db
hiromuota166 <otahiromu166@gmail.com>
2023-12-11 16:10:36 +0900

    チャッピーに聞いた

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     options = Options()
-    options.headless = True  # ヘッドレスモード True=非表示, False=表示
+    options.headless = True
     driver = webdriver.Chrome(options=options)
     driver.get("https://www.reserve1.jp/yoyaku/member/MemberReserve.php?mn=2&lc=wrxqxwrrxx&gr=2&stt=MLRRX")
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    return soup
+    driver.quit()  # ブラウザを閉じる
+    return soup.prettify()  # HTMLコンテンツを整形して文字列として返す
