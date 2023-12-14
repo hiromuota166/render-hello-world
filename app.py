5b42ffbe87c650cd0d7ea165af8e3a075f2930ca
hiromuota166 <otahiromu166@gmail.com>
2023-12-14 18:18:05 +0900

    requests bs4

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    url = 'https://www.yomiuri.co.jp'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    return soup.title.string
